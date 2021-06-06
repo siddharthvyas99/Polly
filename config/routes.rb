ab9eb151ed717ab72a26c380019fde42b73fc76f
Siddharth Vyas <siddharth.vyas9299@gmail.com>
2021-06-06 23:07:22 +0530

    Added Heroku configurations, and Sidekiq support

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username),
+                                                ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_USERNAME'])) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password),
+                                                  ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_PASSWORD']))
+  end
+  mount Sidekiq::Web, at: '/sidekiq'
   
   resources :users, only: %i[create index]
   resource :sessions, only: %i[create destroy]
